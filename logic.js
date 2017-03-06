959d53ed69641b411583af1473b3274f38988a06
osallyjoo <osallyjoo@gmail.com>
2017-03-06 15:32:22 -0800

    html css

---
+++ logic.js
@@ -0,0 +1,13 @@
+
+<script src="https://www.gstatic.com/firebasejs/3.7.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAZZg0FU5ycDBT4WxJWxc3i6mx9iwsJWJU",
+    authDomain: "trainscheduler-97ea8.firebaseapp.com",
+    databaseURL: "https://trainscheduler-97ea8.firebaseio.com",
+    storageBucket: "trainscheduler-97ea8.appspot.com",
+    messagingSenderId: "350977295816"
+  };
+  firebase.initializeApp(config);
+</script>
